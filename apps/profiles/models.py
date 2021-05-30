3fd0c957ccd9ef32ef7d0f4979d99af4ca1b4526
Adrian Carrascal <acarrascalgarcia@gmail.com>
2021-05-29 20:25:51 -0500

    Set django project and add accounts

---
+++ apps/profiles/models.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Profile(models.Model):
+    uuid = models.UUIDField(
+        verbose_name=_('uuid'),
+        unique=True,
+        blank=False,
+        null=False
+    )
+    first_name = models.CharField(
+        verbose_name=_('first name'),
+        max_length=30,
+        blank=False,
+        null=False
+    )
+    middle_name = models.CharField(
+        verbose_name=_('middle name'),
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    last_name = models.CharField(
+        verbose_name=_('last name'),
+        max_length=60,
+        blank=False,
+        null=False
+    )
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('user'),
+        related_name='profile',
+        blank=False,
+        null=False,
+        on_delete=models.DO_NOTHING
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __unicode__(self):
+        return self
